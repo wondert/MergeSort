df08dfccded8f9b80c35ef1d6844bc906a5b63e9
wondert <rgomezamaro1@gmail.com>
2017-07-31 00:34:56 -0700

    splitseq added; splitsort refactored
    Wrote splitseq as a generic function. Refactored splitsort to a generic sorting function of a sequence by splitting into n blocks for the mergesort algorithm.

--- merged.py
+++ merged.py
@@ -1,10 +1,10 @@
-'''The merged module implements the mergesort algorithm.'''
+"""The merged module implements the mergesort algorithm."""
 
-from dupsort import recursivesort,
+from dupsort import recursivesort
 
 
 def merged(*sequences):
-    '''merge n sequences into single sequence iteratively by index.'''
+    """Merge n sequences into single sequence iteratively by index."""
     # build new sequnce from n blocks. blocks not required to be same length.
     static_length = len(sequences[0])
     mergedsequence = []
@@ -23,23 +23,147 @@ def merged(*sequences):
 
 
 def splitseq(reseq, blocks=2):
-    '''splits a sequence into n blocks of equal size'''
+    """Splits a sequence into n blocks. returns a tuple of n lists."""
+    # TODO refactor to a generator function
+    # split sequence into n blocks of equal size
+    if len(reseq) % blocks == 0:
+        block_size = len(reseq)//blocks
+        index = 0
+        sections = []
+        for _ in range(blocks):
+            sections.append(reseq[index:index+block_size])
+            index += block_size
+        return tuple(item for item in sections)
     # split sequence into n blocks
+    # remaining items are sequentially split between blocks
+    else:
+        block_size = len(reseq)//blocks
+        index = 0
+        sections = []
+        last_piece = []
+        for _ in range(blocks):
+            try:
+                sections.append(reseq[index:index+block_size])
+                index += block_size
+            except IndexError:
+                break
+        if index != len(reseq):
+            for item in range(index, index+block_size-1):
+                try:
+                    last_piece.append(reseq[item])
+                except IndexError:
+                    pass
+            for i in range(len(last_piece)):
+                sections[i].append(last_piece[i])
+        return tuple(item for item in sections)
 
-    # assign those blocks to a tuple, splitseqs, with seqX as name.
 
-    return splitseqs
-
-
-def splitsort(randomseq, key=None, reverse=False):
-    '''splitsort splits a sequence in half, recursively sorts, then merges.'''
-
-    # consider refactoring into generic functions to handle n sequences.
+def generic_splitsort(randomseq, blocks=2, key=None, reverse=False):
+    """Splits a sequence into n blocks, recursively sorts, then merges."""
+    # TODO refactor into generic sorting function that handles n sequences.
     if key:
         # generate mappings to recall original index, keys, values
         keyedseq = [(key(element), index)
                     for index, element in enumerate(randomseq)]
-        # maybe come up with a function to do this step...
+
+        # split keyed sequence into n blocks
+        # TODO - does splitseq work with keyedseq tuple elements???
+        kseq = splitseq(keyedseq, blocks)
+
+        # set globals to track value swaps
+        switch = 0
+        compares = 0
+        flip = 1
+
+        # setup infinite loop
+        while True:
+            # recursively sort each sequence
+            if flip:     # Case1 - ensure the first sort
+                sortedseq = []
+                for index, item in enumerate(kseq):
+                    seq, switch = recursivesort(item)
+                    sortedseq.append(seq)
+                    compares += switch
+                # TODO - check if statement below gives expected result...
+                # TODO - since seq is actually a tuple
+                reseq = merged(*tuple(element for element in sortedseq))
+                kseq = splitseq(reseq, blocks)
+                flip = 0
+
+            elif compares:       # Case2 - keep sorting if last loop had sorts
+                compares = 0
+                sortedseq = []
+                for index, item in enumerate(kseq):
+                    seq, switch = recursivesort(item)
+                    sortedseq.append(seq)
+                    compares += switch
+                # TODO - check if statement below gives expected result...
+                # TODO - since seq is actually a tuple
+                reseq = merged(*tuple(element for element in sortedseq))
+                kseq = splitseq(reseq, blocks)
+
+            else:               # Case3 - no sorts
+                break
+
+        # recover keyed elements and create sorted sequence
+        # seq1 = [randomseq[seq1[element][1]] for element in range(len(seq1))]
+        # TODO - make a generic function to do the procedure above
+        # TODO - can we just use merged then recover all elements? YES!!!
+        seq_keyed = merged(*kseq)
+        kseq = [randomseq[seq_keyed[element][1]]
+                for element in range(len(seq_keyed))]
+
+    else:
+        # split sequence into n blocks
+        kseq = splitseq(randomseq, blocks)
+
+        # set globals to track value swaps
+        switch = 0
+        compares = 0
+        flip = 1
+
+        # setup infinite loop
+        while True:
+            # recursively sort each sequence
+            if flip:     # Case1 - ensure the first sort
+                sortedseq = []
+                for index, item in enumerate(kseq):
+                    seq, switch = recursivesort(item)
+                    sortedseq.append(seq)
+                    compares += switch
+                reseq = merged(*tuple(element for element in sortedseq))
+                kseq = splitseq(reseq, blocks)
+                flip = 0
+
+            elif compares:       # Case2 - keep sorting if last loop had sorts
+                compares = 0
+                sortedseq = []
+                for index, item in enumerate(kseq):
+                    seq, switch = recursivesort(item)
+                    sortedseq.append(seq)
+                    compares += switch
+                reseq = merged(*tuple(element for element in sortedseq))
+                kseq = splitseq(reseq, blocks)
+
+            else:               # Case3 - no sorts
+                break
+
+        seq = merged(*kseq)
+
+    # combine sorted sequences, check if reverse, and return sorted sequence
+    seq = merged(*kseq)
+    if reverse:
+        seq.reverse()
+    return seq
+
+
+def splitsort(randomseq, key=None, reverse=False):
+    """Splits a sequence into two blocks, recursively sorts, then merges."""
+    if key:
+        # generate mappings to recall original index, keys, values
+        keyedseq = [(key(element), index)
+                    for index, element in enumerate(randomseq)]
+
         seq1 = keyedseq[:len(keyedseq)//2]
         seq2 = keyedseq[(len(keyedseq)//2):]
 
@@ -53,7 +177,6 @@ def splitsort(randomseq, key=None, reverse=False):
         while True:
             # recursively sort each sequence
             compares = switch1 + switch2
-            # TODO - make a function to recursivesort n sequences as below
             if flip:     # Case1 - ensure the first sort
                 seq1, switch1 = recursivesort(seq1)
                 seq2, switch2 = recursivesort(seq2)
@@ -73,13 +196,13 @@ def splitsort(randomseq, key=None, reverse=False):
         # recover keyed elements and create sorted sequence
         seq1 = [randomseq[seq1[element][1]] for element in range(len(seq1))]
         seq2 = [randomseq[seq2[element][1]] for element in range(len(seq2))]
-        # TODO - make a function to do the procedure above
 
     else:
-        # DO STUFF
+        # TODO refactor above for non-keyed sequence
+        pass
 
     # combine sorted sequences, check if reverse, and return sorted sequence
-    seq = seq1+seq2
+    seq = seq1 + seq2
     if reverse:
         seq.reverse()
     return seq
